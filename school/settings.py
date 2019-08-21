69f864b46a45365bd09effaf8f73201e8da28ef9
Isaac <ikiptanui@powergen-re.com>
2019-08-21 21:48:03 +0300

    Initial commit

--- school/settings.py
+++ school/settings.py
@@ -82,22 +82,22 @@ WSGI_APPLICATION = 'school.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME':  'd8lq65lcq3fprr',
-        'HOST':'ec2-184-72-237-95.compute-1.amazonaws.com',
-        'PORT':'5432',
-        'USER':'ttukzcdefstywm',
-        'PASSWORD':'e92a0312baee86e628ea795c0344939f610d56fa582e03273d3328249083267c',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME':  'd8lq65lcq3fprr',
+#         'HOST':'ec2-184-72-237-95.compute-1.amazonaws.com',
+#         'PORT':'5432',
+#         'USER':'ttukzcdefstywm',
+#         'PASSWORD':'e92a0312baee86e628ea795c0344939f610d56fa582e03273d3328249083267c',
+#     }
+# }
 
 
 # Password validation
